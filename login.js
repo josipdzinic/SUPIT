1161fc10cac582e234f13079c2c25b340f95113e
Josip Džinić <jdzinic@racunarstvo.hr>
2023-05-27 01:41:17 +0200

    Added NastavniPlan

--- login.js
+++ login.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
     console.log("Current Password " + account[1]);
 
     const body = {
-      username: account[0],
-      password: account[1]
+      username: loginUsernameEntry,
+      password: loginPasswordEntry
     };
 
     $.ajax({
@@ -20,24 +20,24 @@ $(document).ready(function () {
       data: JSON.stringify(body),
       contentType: "application/json",
       success: function (data) {
-        const tokenObj = data.token; // Extract the token object from the response
+        const tokenObj = data.token; 
         const tokenString = JSON.stringify(tokenObj);
-        localStorage.setItem("token", data.data.token)
-        localStorage.setItem("Username", data.data.username)
+        localStorage.setItem("token", data.data.token);
+        localStorage.setItem("Username", data.data.username);
         console.log("Logged In");
         setTimeout(() => {
           window.location.replace("index.html");
         }, 1000);
-
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log("Login Failed");
+        const errorMessage = "Login failed. Please check your credentials.";
+        $("#error-message").text(errorMessage);
       }
     });
-
-
-
   });
 
   $('#create-button').on('click', function () {
-
     var createPasswordEntry = $("#create-password").val();
     var createEmailEntry = $("#create-email").val();
 
@@ -49,9 +49,7 @@ $(document).ready(function () {
     var validate = /^\s*[a-zA-Z0-9,\s]+\s*$/;
     var validateEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-
-
-    if (!validate.test(createPasswordEntry) || (createPasswordEntry).length == 0) {
+    if (!validate.test(createPasswordEntry) || createPasswordEntry.length === 0) {
       $(createPasswordObject).addClass("error");
       $(createPasswordObject).val("No special characters or spaces.");
     } else {
@@ -64,15 +62,9 @@ $(document).ready(function () {
       $(createEmailObject).val("Enter a valid email");
     } else {
       createEmailValid = true;
-      console.log("Account Email " + createEmailObject.val())
-
-
-      var createEmail = createEmailObject.val()
-
-
+      console.log("Account Email " + createEmailObject.val());
+      var createEmail = createEmailObject.val();
     }
-
-
 
     $(createPasswordObject).on('click', function () {
       $(this).val("");
@@ -83,8 +75,6 @@ $(document).ready(function () {
       $(this).val("");
       $(this).removeClass("error");
     });
-
-
 
     account = [createEmail, createPassword];
     console.log("Account Email " + account[0]);
@@ -105,7 +95,7 @@ $(document).ready(function () {
       }
     });
 
-    if (createPasswordValid == true && createEmailValid == true) {
+    if (createPasswordValid && createEmailValid) {
       $('form').animate({
         height: "toggle",
         opacity: "toggle"
