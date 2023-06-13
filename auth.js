f0c9bdd81fa21b68154aaeb0be4fdedae045eed4
josipdzinic <jdzinic@racunarstvo.hr>
2023-06-13 08:38:36 +0200

    Added animations

--- auth.js
+++ auth.js
@@ -31,18 +31,18 @@ function logout()
 function contact()
 {
     
-  
-  // contact form animations
+
   $('#contact').click(function() {
     $('#contactForm').fadeIn();
   })
   $(document).mouseup(function (e) {
     var container = $("#contactForm");
 
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    if (!container.is(e.target) 
+        && container.has(e.target).length === 0) 
     {
         container.fadeOut();
     }
   });
 }
+
